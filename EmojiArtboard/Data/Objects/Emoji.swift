0c62ad7dfc731542df1cf25ecbd4d899693fd31b
g-ruffo <grayson@veltus.ca>
2023-01-17 12:45:15 -0800

    feat: Create Emoji object

---
+++ EmojiArtboard/Data/Objects/Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-17.
+//
+
+import Foundation
+
+
+struct Emoji {
+    let text: String
+    var x: Int
+    var y: Int
+    var size: Int
+    
+}
