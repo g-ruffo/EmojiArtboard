b7e139394ad9c4acb3453077afefe7d32e31361a
g-ruffo <grayson@veltus.ca>
2023-01-17 13:01:09 -0800

    feat: Implement identifiable and create init

--- EmojiArtboard/Data/Objects/Emoji.swift
+++ EmojiArtboard/Data/Objects/Emoji.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 
-struct Emoji {
+struct Emoji: Identifiable {
     let text: String
     var x: Int
     var y: Int
     var size: Int
-    
+    let id: Int
+
+    fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+        self.text = text
+        self.x = x
+        self.y = y
+        self.size = size
+        self.id = id
+    }
 }
