780b86ff71d483a4bbc91e0652e049129b35996f
g-ruffo <grayson@veltus.ca>
2023-01-17 12:46:36 -0800

    feat: Create artboard model

---
+++ EmojiArtboard/Model/EmojiArtboard.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiArtboard.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-17.
+//
+
+import Foundation
+
+struct EmojiArtboard {
+    var background: Background
+    var emojis = [Emoji]()
+    
+}
