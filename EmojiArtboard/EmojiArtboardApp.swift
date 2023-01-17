3bce7823db14d7fd4371aea21c3ee0f5756b0b02
g-ruffo <grayson@veltus.ca>
2023-01-17 00:24:11 -0800

    Initial Commit

---
+++ EmojiArtboard/EmojiArtboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtboardApp.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
