6256810d12268f3f1ef3683d8abf4047b3fc53c1
g-ruffo <grayson@veltus.ca>
2023-01-17 13:36:31 -0800

    feat: Create extension file

---
+++ EmojiArtboard/Utils/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-17.
+//
+
+import Foundation
+
+extension Collection where Element: Identifiable {
+    func index(matching element: Element) -> Self.Index? {
+        firstIndex { $0.id == element.id }
+    }
+}
