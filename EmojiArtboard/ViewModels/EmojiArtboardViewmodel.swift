2e8a00656513a3e7e0b729034e1d9bef56a1b0bc
g-ruffo <grayson@veltus.ca>
2023-01-17 13:21:16 -0800

    feat: Create view model

---
+++ EmojiArtboard/ViewModels/EmojiArtboardViewmodel.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiArtboardViewmodel.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-17.
+//
+
+import SwiftUI
+
+class EmojiArtboardViewModel: ObservableObject {
+    
+    @Published private(set) var emojiArtboard: EmojiArtboardModel
+}
