ded95763b23254a99881b127ac9d1263d0d8be5c
g-ruffo <grayson@veltus.ca>
2023-01-17 14:01:18 -0800

    style: Create test layout

--- EmojiArtboard/Views/EmojiArtboardView.swift
+++ EmojiArtboard/Views/EmojiArtboardView.swift
@@ -10,9 +10,42 @@ import SwiftUI
 struct EmojiArtboardView: View {
     @ObservedObject var viewModel: EmojiArtboardViewModel
     
+    let defaultEmojiFontSize: CGFloat = 40
+    
     var body: some View {
-
+        VStack(spacing: 0) {
+            documentBody
+            palette
+        }
     }
+
+
+var documentBody: some View {
+    Color.yellow
+}
+
+var palette: some View {
+    ScrollingEmojisView(emojis: testEmojis)
+        .font(.system(size: defaultEmojiFontSize))
+}
+
+
+
+let testEmojis = "😀😃😄😁😆😅😂🤣🥲🥹☺️😊😇🙂🙃😉😌😍"
+
+struct ScrollingEmojisView: View {
+    let emojis: String
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis.map { String($0) }, id: \.self) { emoji in
+                    Text(emoji)
+                }
+            }
+        }
+    }
+}
 }
 
 struct EmojiArtboardView_Previews: PreviewProvider {
