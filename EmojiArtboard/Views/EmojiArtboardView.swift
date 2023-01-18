3c36c770e58ce05470f8285f0e31270f0ab1a7ee
g-ruffo <grayson@veltus.ca>
2023-01-17 17:36:01 -0800

    feat: Implement emoji drag and drop

--- EmojiArtboard/Views/EmojiArtboardView.swift
+++ EmojiArtboard/Views/EmojiArtboardView.swift
@@ -18,34 +18,80 @@ struct EmojiArtboardView: View {
             palette
         }
     }
-
-
-var documentBody: some View {
-    Color.yellow
-}
-
-var palette: some View {
-    ScrollingEmojisView(emojis: testEmojis)
-        .font(.system(size: defaultEmojiFontSize))
-}
-
-
-
-let testEmojis = "😀😃😄😁😆😅😂🤣🥲🥹☺️😊😇🙂🙃😉😌😍"
-
-struct ScrollingEmojisView: View {
-    let emojis: String
     
-    var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(emojis.map { String($0) }, id: \.self) { emoji in
-                    Text(emoji)
+    
+    var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.yellow
+                ForEach(viewModel.emojis) { emoji in
+                    Text(emoji.text)
+                        .font(.system(size: fontSize(for: emoji)))
+                        .position(position(for: emoji, in: geometry))
+                }
+                
+            }
+            .onDrop(of: [.plainText], isTargeted: nil) { providers, location in
+                drop(providers: providers, at: location, in: geometry)
+            }
+        }
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        return providers.loadObjects(ofType: String.self) { string in
+            if let emoji = string.first, emoji.isEmoji {
+                viewModel.addEmoji(String(emoji), at: convertToEmojiCoordinates(location, in: geometry), size: defaultEmojiFontSize)
+            }
+        }
+    }
+    
+    private func position(for emoji: Emoji, in geometry: GeometryProxy) -> CGPoint {
+        convertFromEmojiCoordinates((x: emoji.x, y: emoji.y), in: geometry)
+    }
+    
+    private func convertToEmojiCoordinates(_ location: CGPoint, in geometry: GeometryProxy) -> (x: Int, y: Int) {
+        let center = geometry.frame(in: .local).center
+        let location = CGPoint (
+        x: location.x - center.x,
+        y: location.y - center.y
+        )
+        return (Int(location.x), Int(location.y))
+    }
+    
+    private func convertFromEmojiCoordinates(_ location: (x: Int, y: Int), in geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(
+            x: center.x + CGFloat(location.x),
+            y: center.y + CGFloat(location.y))
+    }
+    
+    private func fontSize(for emoji: Emoji) -> CGFloat {
+        CGFloat(emoji.size)
+    }
+    
+    
+    var palette: some View {
+        ScrollingEmojisView(emojis: testEmojis)
+            .font(.system(size: defaultEmojiFontSize))
+    }
+    
+    
+    let testEmojis = "😀😃😄😁😆😅😂🤣🥲🥹☺️😊😇🙂🙃😉😌😍"
+    
+    struct ScrollingEmojisView: View {
+        let emojis: String
+        
+        var body: some View {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(emojis.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .onDrag { NSItemProvider(object: emoji as NSString) }
+                    }
                 }
             }
         }
     }
-}
 }
 
 struct EmojiArtboardView_Previews: PreviewProvider {
