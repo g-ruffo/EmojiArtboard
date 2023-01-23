e835376c6b6838030d8d1d2d15e6a885e5282cd7
g-ruffo <grayson@veltus.ca>
2023-01-23 14:19:55 -0800

    feat: Create function to add CGSize

--- EmojiArtboard/Utils/Extensions.swift
+++ EmojiArtboard/Utils/Extensions.swift
@@ -80,3 +80,22 @@ extension URL {
     }
 }
 
+extension CGSize {
+    // The center point of an area that is our size
+    var center: CGPoint {
+        CGPoint(x: width/2, y: height/2)
+    }
+    static func +(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+    }
+    static func *(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+    }
+    static func /(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+    }
+}
+
