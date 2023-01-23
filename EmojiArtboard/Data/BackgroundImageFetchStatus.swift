1a78fb7f06ffa8674706fc466ee1c13925754591
g-ruffo <grayson@veltus.ca>
2023-01-23 12:14:49 -0800

    chore: Create loading enum

---
+++ EmojiArtboard/Data/BackgroundImageFetchStatus.swift
@@ -0,0 +1,13 @@
+//
+//  BackgroundImageFetchStatus.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-23.
+//
+
+import Foundation
+
+    enum BackgroundImageFetchStatus {
+        case idle
+        case fetching
+    }
