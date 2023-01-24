7a1d7794a221886bbcb140e141246bd7472bcbf7
g-ruffo <grayson@veltus.ca>
2023-01-23 18:44:09 -0800

    feat: Create modifier for emoji selection

---
+++ EmojiArtboard/Utils/SelectionEffect.swift
@@ -0,0 +1,26 @@
+//
+//  SelectionEffect.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-23.
+//
+
+import SwiftUI
+
+struct SelectionEffect: ViewModifier {
+    var emoji: Emoji
+    var selectedEmojis: Set<Emoji>
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                selectedEmojis.contains(emoji) ? RoundedRectangle(cornerRadius: 0).strokeBorder(lineWidth: 5).foregroundColor(.green) : nil
+            )
+    }
+}
+
+extension View {
+    func selectionEffect(for emoji: Emoji, in selectedEmojis: Set<Emoji>) -> some View {
+        modifier(SelectionEffect(emoji: emoji, selectedEmojis: selectedEmojis))
+    }
+}
