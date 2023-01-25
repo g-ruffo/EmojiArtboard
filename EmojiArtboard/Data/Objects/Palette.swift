813973100680590877e991f20ee6c9ee6df8070d
g-ruffo <grayson@veltus.ca>
2023-01-25 12:52:34 -0800

    feat: Set Palette as codable

--- EmojiArtboard/Data/Objects/Palette.swift
+++ EmojiArtboard/Data/Objects/Palette.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 
-struct Palette: Identifiable {
+struct Palette: Identifiable, Codable {
     var name: String
     var emojis: String
     var id: Int
+    
+    init(name: String, emojis: String, id: Int) {
+        self.name = name
+        self.emojis = emojis
+        self.id = id
+    }
 }
