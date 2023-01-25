51346dac507a9feede6936972258ec774654a43d
g-ruffo <grayson@veltus.ca>
2023-01-24 19:00:57 -0800

    chore: Create palette object

---
+++ EmojiArtboard/Data/Objects/Palette.swift
@@ -0,0 +1,14 @@
+//
+//  Palette.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-24.
+//
+
+import Foundation
+
+struct Palette: Identifiable {
+    var name: String
+    var emojis: String
+    var id: Int
+}
