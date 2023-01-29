967574223e7825a8a11d292952c24ce9e942a9f1
g-ruffo <grayson@veltus.ca>
2023-01-29 14:18:30 -0800

    feat: Create manager view

---
+++ EmojiArtboard/Screens/Views/PaletteManagerView.swift
@@ -0,0 +1,63 @@
+//
+//  PaletteManagerView.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-25.
+//
+
+import SwiftUI
+
+struct PaletteManagerView: View {
+    @EnvironmentObject var viewModel: PaletteStoreViewModel
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.presentationMode) var presentationMode
+    @State private var editMode: EditMode = .inactive
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.palettes) { palette in
+                    NavigationLink(destination: PaletteEditorView(palette: $viewModel.palettes[palette])) {
+                        VStack(alignment: .leading) {
+                            Text(palette.name)
+                            Text(palette.emojis)
+                        }
+                        .gesture(editMode == .active ? tap : nil)
+                    }
+                }
+                .onDelete { indexSet in
+                    viewModel.palettes.remove(atOffsets: indexSet)
+                }
+                .onMove { indexSet, newOffset in
+                    viewModel.palettes.move(fromOffsets: indexSet, toOffset: newOffset)
+                }
+            }
+            .navigationTitle("Manage Palettes")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem { EditButton() }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    if presentationMode.wrappedValue.isPresented,
+                       UIDevice.current.userInterfaceIdiom != .pad {
+                        Button("Close") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            }
+            .environment(\.editMode, $editMode)
+        }
+    }
+    
+    var tap: some Gesture {
+        TapGesture().onEnded { }
+    }
+}
+
+struct PaletteManagerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteManagerView()
+            .environmentObject(PaletteStoreViewModel(named: "Preview"))
+            .preferredColorScheme(.light)
+    }
+}
