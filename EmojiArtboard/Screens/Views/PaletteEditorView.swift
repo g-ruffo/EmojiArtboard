a0e7d6d607523dbfd6059167e251a6c7972f0fac
g-ruffo <grayson@veltus.ca>
2023-01-29 14:20:02 -0800

    feat: Create add and remove function

--- EmojiArtboard/Screens/Views/PaletteEditorView.swift
+++ EmojiArtboard/Screens/Views/PaletteEditorView.swift
@@ -8,19 +8,64 @@
 import SwiftUI
 
 struct PaletteEditorView: View {
-    @State private var palette: Palette = PaletteStoreViewModel(named: "Test").palette(at: 2)
+    @Binding var palette: Palette
     
     var body: some View {
         Form {
+            nameSection
+            addEmojisSection
+            removeEmojisSection
+        }
+        .navigationTitle("Edit \(palette.name)")
+        .frame(minWidth: 300, minHeight: 350)
+    }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")) {
             TextField("Name", text: $palette.name)
         }
-        .frame(minWidth: 300, minHeight: 350)
+    }
+    
+    @State private var emojisToAdd = ""
+
+var addEmojisSection: some View {
+    Section(header: Text("Add Emojies")) {
+        TextField("", text: $emojisToAdd)
+            .onChange(of: emojisToAdd) { emojis in
+                addEmojis(emojis)
+            }
+    }
+}
+    
+    var removeEmojisSection: some View {
+        Section(header: Text("Remove Emojies")) {
+            let emojis = palette.emojis.map { String($0)}
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                palette.emojis.removeAll(where: { String($0) == emoji })
+                            }
+                        }
+                }
+            }
+            TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    func addEmojis(_ emojis: String) {
+        palette.emojis = (emojis + palette.emojis)
+            .filter { $0.isEmoji}
     }
 }
 
 struct PaletteEditorView_Previews: PreviewProvider {
     static var previews: some View {
-        PaletteEditorView()
+        PaletteEditorView(palette: .constant(PaletteStoreViewModel(named: "Preview").palette(at: 4)))
             .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/))
     }
 }
