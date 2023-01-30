2fbc859b596a58cd95cdd4b0b280b9998e63907b
g-ruffo <grayson@veltus.ca>
2023-01-29 18:07:40 -0800

    feat: Create extension for converting scene storage value

--- EmojiArtboard/Utils/Extensions.swift
+++ EmojiArtboard/Utils/Extensions.swift
@@ -164,3 +164,23 @@ extension DragGesture.Value {
     var distance: CGSize { location - startLocation }
 }
 
+extension CGSize: RawRepresentable { }
+
+extension CGFloat: RawRepresentable { }
+
+extension RawRepresentable where Self: Codable {
+    public var rawValue: String {
+        if let json = try? JSONEncoder().encode(self), let string = String(data: json, encoding: .utf8) {
+            return string
+        } else {
+            return ""
+        }
+    }
+    public init?(rawValue: String) {
+        if let value = try? JSONDecoder().decode(Self.self, from: Data(rawValue.utf8)) {
+            self = value
+        } else {
+            return nil
+        }
+    }
+}
