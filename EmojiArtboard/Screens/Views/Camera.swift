4f6158b60daada7b725f6ae2e9f48aa6e5c0ef3a
g-ruffo <grayson@veltus.ca>
2023-01-30 15:42:49 -0800

    feat: Create camera controller

---
+++ EmojiArtboard/Screens/Views/Camera.swift
@@ -0,0 +1,49 @@
+//
+//  Camera.swift
+//  EmojiArtboard
+//
+//  Created by Grayson Ruffo on 2023-01-30.
+//
+
+import SwiftUI
+
+struct Camera: UIViewControllerRepresentable {
+    
+    var handlePickedImage: (UIImage?) -> Void
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(handlePickedImage: handlePickedImage)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var handlePickedImage: (UIImage?) -> Void
+        
+        init(handlePickedImage: @escaping (UIImage?) -> Void) {
+            self.handlePickedImage = handlePickedImage
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            handlePickedImage(nil)
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            handlePickedImage((info[.editedImage] ?? info[.originalImage]) as? UIImage)
+        }
+    }
+    
+    
+    static var isAvailable: Bool {
+        UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+}
